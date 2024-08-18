549596ded09528a0b898f4526bfc257cd853374f
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2024-08-18 19:47:17 -0300

    Explorando os tipos de dados em Java

--- dio-trilha-java-basico/tipos-variaveis/src/TipoVariaveis.java
+++ dio-trilha-java-basico/tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,23 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //estudar sobre -->
+        //tipos primitivos e String
+     
+
+        String nome = "Lucas Fernandes";
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 50;
+        numero = 10;
+        System.out.println(numero);
+
+        final double PI = 3.14;
+        
+
+
+
     }
 }
