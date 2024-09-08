6871113be57dc11368f871a341c072d56bb67297
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2024-09-08 07:57:21 -0300

    Criando o diretorio estrutura-repetição,

---
+++ dio-trilha-java-basico/estrutura-repeticao/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        for(int carneirinho = 1; carneirinho <=20;carneirinho++){
+             System.out.println("carneirinho = " + carneirinho);
+
+        }
+        System.out.println("Ele dormiuuu xxxxiu");
+    }
+}
---
+++ dio-trilha-java-basico/estrutura-repeticao/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        
+        String alunos[] = {"Jão","Julia","Jonas"};
+
+        for(int cont=0; cont < alunos.length; cont++){
+            System.out.println("\nAluno no indice x=" +  cont + " - " + alunos[cont]);
+
+        }
+    }
+    
+}
